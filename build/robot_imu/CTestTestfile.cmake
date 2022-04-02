1a64e376dbfabc09383d42ee2f45835e18618153
LabRobotica <sanrega24@gmail.com>
2022-04-01 20:11:31 -0500

    ROS architecture begun to be changed to services and to deprecate rosserial.

--- build/robot_imu/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/uao/3drobot/src/robot_imu
-# Build directory: /home/uao/3drobot/build/robot_imu
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
